4a3fed061300ad17ac4b6b4c47aad0bf096a319e
roisu <roisusi@gmail.com>
2021-02-15 19:37:24 +0200

    ServerFinish-Client Creation

--- EchoServer/src/ServerClient/Main.java
+++ EchoServer/src/ServerClient/Main.java
@@ -1,8 +1,34 @@
 package ServerClient;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            //Communicate with the Client
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Accepted");
+            //Read from the Server Socket
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+
+            while (true){
+                String echoString = input.readLine();
+                if(echoString.equals("exit")){
+                    break;
+                }
+                output.println("Echo from server: " + echoString);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Server exceptions" + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
 }
